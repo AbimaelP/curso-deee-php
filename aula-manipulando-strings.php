f03b4debb8cebf33c309671284214e9f0ab724e3
AbimaelP <a.bimael2000@hotmail.com>
2022-07-23 21:53:39 -0300

    aula-manipulando-strings.php

---
+++ aula-manipulando-strings.php
@@ -0,0 +1,7 @@
+<?php
+    $texto = "O rato roeu a roupa do rei de roma";
+    echo strlen($texto) ."<br/>";
+    echo str_word_count($texto) ."<br/>";
+    echo strrev($texto) ."<br/>";
+    echo strpos($texto, "rei") ."<br/>";
+?>
