0cc142022c161af26b1ff9262350e784727c93bf
AbimaelP <a.bimael2000@hotmail.com>
2022-07-24 10:32:07 -0300

    matrizes,contantes,foreach,superglobais

--- constantes.php
+++ constantes.php
@@ -1,3 +1,5 @@
-<?php
-
+<?
+//constantes funcionam de forma global
+    define("cpf", 123456789);
+    echo cpf;
 ?>
---
+++ matrizes.php
@@ -0,0 +1,9 @@
+<?php
+    //$carros = ["uno","gol","celta","palio"];
+    //$nCarros = count($carros);
+    //echo $nCarros;
+    $pessoas = ["Nome"=>"Abimael","idade"=>21,"Tamanho"=>1.60];
+    //sort() asort() organiza pelo valor ksort()organiza pela chave arsort() krsort() do maior para o menor
+    //$pessoas["CPF"] = 50402797876;
+    echo $pessoas["Nome"];
+?>
